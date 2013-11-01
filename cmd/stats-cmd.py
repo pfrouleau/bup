8dede5d832f4ac89687ec9fa144e8924fde14924
Patryck Rouleau <prouleau72@gmail.com>
2013-10-31 23:36:48 -0400

    stats-cmd: add a show option to show the blobs of a file

--- cmd/stats-cmd.py
+++ cmd/stats-cmd.py
@@ -4,20 +4,13 @@ from bup import git, options
 from bup.helpers import *
 
 git.check_repo_or_die()
-
 db_path = git.repo('bupstats.sqlite3')
+db = None
 
-if os.path.exists(db_path):
-    os.unlink(db_path)
-
-db = sqlite3.connect(db_path)
-db.execute('CREATE TABLE objects (sha text, size integer);')
-db.execute('CREATE TABLE refs (a text, b text);')
-
-DONT_SKIP_KNOWN = False
+SKIP_KNOWN = True
 
 def traverse_commit(cp, sha_hex, needed_objects):
-    if sha_hex not in needed_objects or DONT_SKIP_KNOWN:
+    if sha_hex not in needed_objects or not SKIP_KNOWN:
         needed_objects.add(sha_hex)
 
         it = iter(cp.get(sha_hex))
@@ -25,18 +18,18 @@ def traverse_commit(cp, sha_hex, needed_objects):
         assert(type == 'commit')
         content = "".join(it)
         tree_sha = content.split("\n")[0][5:].rstrip(" ")
-        db.execute('INSERT INTO refs VALUES (?,?)', (sha_hex, tree_sha))
+        db.execute('INSERT INTO refs VALUES (?,?,?,?)', (sha_hex, tree_sha, 0,'commit'))
         sum = len(content)
         for (t,s,c,l) in traverse_objects(cp, tree_sha, needed_objects):
             sum += c
             yield (t,s,c,l)
-            db.execute('INSERT INTO objects VALUES (?,?)', (s, c))
+            db.execute('INSERT INTO objects VALUES (?,?,?)', (s, t, c))
+        db.execute('INSERT INTO objects VALUES (?,?,?)', (sha_hex, type, sum))
         yield ('commit', sha_hex, sum, len(content))
-        db.execute('INSERT INTO objects VALUES (?,?)', (sha_hex, sum))
 
 
 def traverse_objects(cp, sha_hex, needed_objects):
-    if sha_hex not in needed_objects or DONT_SKIP_KNOWN:
+    if sha_hex not in needed_objects or not SKIP_KNOWN:
         needed_objects.add(sha_hex)
         it = iter(cp.get(sha_hex))
         type = it.next()
@@ -54,7 +47,7 @@ def traverse_objects(cp, sha_hex, needed_objects):
             content = "".join(it)
             sum = len(content)
             for (mode,mangled_name,sha) in git.tree_decode(content):
-                db.execute('INSERT INTO refs VALUES (?,?)', (sha_hex, sha.encode('hex')))
+                db.execute('INSERT INTO refs VALUES (?,?,?,?)', (sha_hex, sha.encode('hex'), mode, mangled_name))
 
                 for (t,s,c,l) in traverse_objects(cp, sha.encode('hex'),
                                             needed_objects):
@@ -66,9 +59,96 @@ def traverse_objects(cp, sha_hex, needed_objects):
             content = "".join(it)
             yield ('blob', sha_hex, len(content), len(content))
 
+
+def fill_database():
+    global db
+    if os.path.exists(db_path):
+        os.unlink(db_path)
+
+    db = sqlite3.connect(db_path)
+    db.execute('CREATE TABLE objects (sha text, type text, size integer);')
+    db.execute('CREATE TABLE refs (a text, b text, mode integer, name text);')
+
+    cp = git.CatPipe()
+
+    opt.progress = (istty2 and not opt.quiet)
+    refs = git.list_refs()
+    refnames = [name for name, sha in refs]
+
+    pl = git.PackIdxList(git.repo('objects/pack'))
+
+    needed_objects = git.NeededObjects(pl)
+
+    # Find needed objects reachable from commits
+    traversed_objects_counter = 0
+
+    for refname in refnames:
+        if not refname.startswith('refs/heads/'):
+            continue
+        log('Traversing %s to find needed objects...\n' % refname[11:])
+        for date, sha in ((date, sha.encode('hex')) for date, sha in
+                          git.rev_list(refname)):
+            for type, sha_, sum, size in traverse_commit(cp, sha, needed_objects):
+                qprogress('                             \r')
+                sys.stdout.write("%s\t%s\t%d\t%d\n" % (type, sha_, sum, size))
+                traversed_objects_counter += 1
+                qprogress('Traversing objects: %d\r' % traversed_objects_counter)
+
+    # Find needed objects reachable from tags
+    tags = git.tags()
+    if len(tags) > 0:
+        for key in tags:
+            log('Traversing tag %s to find needed objects...\n' % ", ".join(tags[key]))
+            for type, sha, sum, size in traverse_commit(cp, sha, needed_objects):
+                sys.stdout.write("%s\t%s\t%d\t%d\n" % (type, sha_, sum, size))
+                traversed_objects_counter += 1
+                qprogress('Traversing objects: %d\r' % traversed_objects_counter)
+    progress('Traversing objects: %d, done.\n' % traversed_objects_counter)
+
+    db.commit()
+
+    if traversed_objects_counter == 0:
+        o.fatal('No reachable objects found.')
+
+
+def _show_blobs(db, hash, ofs):
+    #print("# %12d %s" % (ofs, hash))
+    cur = db.cursor()
+    #db.execute('CREATE TABLE objects (sha text, type text, size integer);')
+    #db.execute('CREATE TABLE refs (a text, b text);')
+    cur.execute('select o.sha, o.type, o.size, r.name, r.mode from objects o join refs r where r.a=:h and r.b=o.sha',
+                {"h": hash})
+    total = ofs
+    for sha, type, size, name, mode in cur.fetchall():
+        #print("# %s %s %X %12d %s" % (sha, type, mode, size, name))
+        if type == 'tree':
+            for total1, sha1, size1, ofs1 in _show_blobs(db, sha, total):
+                total = total1
+                yield (total, sha1, size1, ofs1)
+        elif type == 'blob':
+            total += size
+            yield (total, sha, size, ofs)
+            ofs += size
+
+
+def show_blobs():
+    print "# hash=%s" % opt.show
+    print "#"
+    db = sqlite3.connect(db_path)
+    t = 0
+    for total, sha, size, ofs in _show_blobs(db, opt.show, t):
+        print("%12d %12d %s" % (size, ofs, sha))
+        sys.stdout.flush()
+        t = total
+    print "#------------------------"
+    print "# Total =    %12d" % t
+
+
 optspec = """
 bup stats
 --
+f,reset    reset the database
+s,show=    show blobs for hash
 q,quiet    don't show progress meter
 """
 o = options.Options(optspec)
@@ -76,43 +156,9 @@ o = options.Options(optspec)
 
 handle_ctrl_c()
 
-cp = git.CatPipe()
-
-opt.progress = (istty2 and not opt.quiet)
-refs = git.list_refs()
-refnames = [name for name, sha in refs]
-
-pl = git.PackIdxList(git.repo('objects/pack'))
-
-needed_objects = git.NeededObjects(pl)
-
-# Find needed objects reachable from commits
-traversed_objects_counter = 0
-
-for refname in refnames:
-    if not refname.startswith('refs/heads/'):
-        continue
-    log('Traversing %s to find needed objects...\n' % refname[11:])
-    for date, sha in ((date, sha.encode('hex')) for date, sha in
-                      git.rev_list(refname)):
-        for type, sha_, sum, size in traverse_commit(cp, sha, needed_objects):
-            sys.stdout.write("%s\t%s\t%d\t%d\n" % (type, sha_, sum, size))
-            traversed_objects_counter += 1
-            qprogress('Traversing objects: %d\r' % traversed_objects_counter)
-
-# Find needed objects reachable from tags
-tags = git.tags()
-if len(tags) > 0:
-    for key in tags:
-        log('Traversing tag %s to find needed objects...\n' % ", ".join(tags[key]))
-        for type, sha, sum, size in traverse_commit(cp, sha, needed_objects):
-            sys.stdout.write("%s\t%s\t%d\t%d\n" % (type, sha_, sum, size))
-            traversed_objects_counter += 1
-            qprogress('Traversing objects: %d\r' % traversed_objects_counter)
-progress('Traversing objects: %d, done.\n' % traversed_objects_counter)
-
-
-if traversed_objects_counter == 0:
-    o.fatal('No reachable objects found.')
-
-db.commit()
+if opt.show:
+    show_blobs()
+elif opt.reset:
+    fill_database()
+else:
+    print "Nothing to do"
